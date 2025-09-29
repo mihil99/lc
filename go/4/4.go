cb2e9eef95f214111a74c5f2e3da76e9919d0989
Michiel DW <michiel.dewilde@hotmail.com>
2025-09-29 22:04:57 +0200

    Updating 4 with correct solution

--- go/4/4.go
+++ go/4/4.go
@@ -16,22 +16,48 @@ var testCases = []map[string][]int{
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	var i1, i2 int
-	if nums1[0] <= nums2[len(nums2)] {
-		i1 = 0
-		i2 = len(nums2) - 1
-	} else {
-		i1 = len(nums1) - 1
-		i2 = 0
-	}
-
-	// for {
-	// go to center of 2 arrays
-	// incrementing and decrementing depending on relation of 2 values
-	// }
-
+	// binary approach
 	return 0.0
 }
+
+// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+// 	i1 := 0
+// 	i2 := 0
+// 	curr := -1
+// 	prev := -1
+// 	total := len(nums1) + len(nums2)
+//
+// 	// fmt.Println("-------------------------------------------------")
+// 	for i := 0; i <= total/2; i++ {
+// 		// fmt.Printf("(%d/%d)\t[Total %d]  [i1 %d]  [i2 %d]  [curr %d]  [prev %d]\n", i, total/2, total, i1, i2, curr, prev)
+// 		if i1 >= len(nums1) {
+// 			prev = curr
+// 			curr = nums2[i2]
+// 			i2++
+// 			continue
+// 		}
+// 		if i2 >= len(nums2) {
+// 			prev = curr
+// 			curr = nums1[i1]
+// 			i1++
+// 			continue
+// 		}
+// 		if nums1[i1] <= nums2[i2] {
+// 			prev = curr
+// 			curr = nums1[i1]
+// 			i1++
+// 		} else {
+// 			prev = curr
+// 			curr = nums2[i2]
+// 			i2++
+// 		}
+// 	}
+//
+// 	if total%2 == 0 && prev != -1 {
+// 		return float64(curr+prev) / 2
+// 	}
+// 	return float64(curr)
+// }
 
 func main() {
 	for _, testCase := range testCases {
