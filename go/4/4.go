4e1a58f832b4dc0642731f70ec24409897a0045b
Michiel DW <michiel.dewilde@hotmail.com>
2025-09-30 22:18:05 +0200

    Finalizing 4

--- go/4/4.go
+++ go/4/4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 var testCases = []map[string][]int{
@@ -13,11 +14,73 @@ var testCases = []map[string][]int{
 		"nums1": {1, 2},
 		"nums2": {3, 4},
 	},
+	{
+		"nums1": func() []int {
+			nums := make([]int, 1000000) // 1 million elements
+			for i := 0; i < 1000000; i++ {
+				nums[i] = i // Numbers from 0 to 999,999
+			}
+			return nums
+		}(),
+		"nums2": func() []int {
+			nums := make([]int, 1500000) // 1.5 million elements
+			for i := 0; i < 1500000; i++ {
+				nums[i] = i + 500000 // Numbers from 500,000 to 1,999,999
+			}
+			return nums
+		}(),
+	},
+}
+
+func newIndex(left int, right int) int {
+	return (left + right) / 2
+}
+
+func getValueFromArray(index int, nums []int) int {
+	if 0 <= index && index < len(nums) {
+		return nums[index]
+	}
+	if index < 0 {
+		return int(-math.Pow(10, 6) - 1)
+	}
+	return int(math.Pow(10, 6) + 1)
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	// binary approach
-	return 0.0
+	// lengthts
+	l1 := len(nums1)
+	l2 := len(nums2)
+
+	low := 0
+	high := len(nums1)
+
+	// partition points,
+	// with the partition point meaning the last value of the left slice of the array
+	b1 := newIndex(low, high)
+	b2 := (l1+l2+1)/2 - b1
+
+	for {
+		maxLeft1 := getValueFromArray(b1-1, nums1)
+		minRight1 := getValueFromArray(b1, nums1)
+		maxLeft2 := getValueFromArray(b2-1, nums2)
+		minRight2 := getValueFromArray(b2, nums2)
+
+		if maxLeft1 <= minRight2 && maxLeft2 <= minRight1 {
+			if (l1+l2)%2 == 0 {
+				return float64(max(maxLeft1, maxLeft2)+min(minRight1, minRight2)) / 2
+			}
+			return float64(max(maxLeft1, maxLeft2))
+		}
+		if maxLeft1 > minRight2 {
+			high = b1
+			b1 = newIndex(low, high)
+			b2 = (l1+l2+1)/2 - b1
+		} else {
+			low = b1 + 1
+			b1 = newIndex(low, high)
+			b2 = (l1+l2+1)/2 - b1
+		}
+	}
 }
 
 // func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
